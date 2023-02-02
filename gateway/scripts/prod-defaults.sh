ba1d95cabb22496b5236aa1be0f6f853e5f5a798
Brian Winkers <bwinkers@gmail.com>
2023-02-01 21:01:27 -0800

    Use generic name for default, temporary cert

--- gateway/scripts/prod-defaults.sh
+++ gateway/scripts/prod-defaults.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-#openssl req -config local.conf -newkey rsa -x509 -days 365 -out local-selfsigned.crt
+#
+# RUN FROM PROJECT ROOT.
+# 
 
-openssl req -config ./gateway/certs/service.ultri.com.conf -newkey rsa:4096 -nodes -keyout ./gateway/certs/default-service.ultri.com.key -x509 -days 365 -out ./gateway/certs/default-service.ultri.com.pem
+openssl req -config ./gateway/certs/service.conf -newkey rsa:4096 -nodes -keyout ./gateway/certs/default-service.key -x509 -days 365 -out ./gateway/certs/default-service.pem
 
 openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:4096 -out ./gateway/config/account.key
