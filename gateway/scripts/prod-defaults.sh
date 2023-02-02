1ebf02bbe9ab2bc0a466553e7412f13a198b6b25
Brian Winkers <bwinkers@gmail.com>
2023-02-01 21:25:50 -0800

    genericize naming for cert files, and ignore them

--- gateway/scripts/prod-defaults.sh
+++ gateway/scripts/prod-defaults.sh
@@ -3,6 +3,6 @@
 # RUN FROM PROJECT ROOT.
 # sh gateway/scripts/prod-defaults.sh
 
-openssl req -config ./gateway/certs/service.conf -newkey rsa:4096 -nodes -keyout ./gateway/certs/default-service.key -x509 -days 365 -out ./gateway/certs/default-service.pem
+openssl req -config ./gateway/certs/service.default.conf -newkey rsa:4096 -nodes -keyout ./gateway/certs/default-service.key -x509 -days 365 -out ./gateway/certs/default-service.pem
 
 openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:4096 -out ./gateway/config/account.key
