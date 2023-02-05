9f46a3d5960afbc4dfbf31a07d2ac6e8164701e4
Brian Winkers <bwinkers@gmail.com>
2023-02-04 22:51:51 -0800

    Init DB and user

---
+++ db/1-create-databases.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+export PGPASSWORD=$POSTGRES_PASSWORD;
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+  BEGIN;
+    CREATE DATABASE $APP_DB_NAME;
+   
+  COMMIT;
+
+EOSQL
