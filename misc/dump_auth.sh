215db9c5bcc84755738cd9f9e874ea85fc0b0bdd
Brian Winkers <bwinkers@gmail.com>
2023-03-14 22:24:04 -0700

    Fixed spelling

--- misc/dump_auth.sh
+++ misc/dump_auth.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # pg_dump -h localhost -U postgres -d izzup -n izzup_api > ./misc/database/izzup_api_schema.sql 
-pg_dump -h localhost -U postgres -d izzup -n ultri_auth > ./misc/database/ulri_auth_schema.sql 
+pg_dump -h localhost -U postgres -d izzup -n ultri_auth > ./misc/database/ultri_auth_schema.sql 
 # pg_dump -h localhost -U postgres -d izzup > ./misc/database/izzup_all.sql
